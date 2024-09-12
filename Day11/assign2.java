6cc8a081990a41903dacce24ff80068b42290d8f
Omkar-kashid-code <omkarkashid.2003@gmail.com>
2024-09-12 19:42:41 +0530

    day.added

---
+++ Day11/assign2.java
@@ -0,0 +1,7 @@
+class Cemox{
+   public static void main(String[] args){
+     for(int i=1 ; i<=20 ; i++){
+       System.out.println("Square of"+ i +":"+ (i * i * i));
+       } 
+    }
+}
---
+++ Day11/assign3.java
@@ -0,0 +1,11 @@
+class Codex{
+   public static void main(String[] args){
+     for(int i=1 ; i<=4 ; i++){
+      for(int j =1;j<=i;j++){
+       System.out.print(0 + " ");
+        }
+
+       System.out.println();
+       } 
+    }
+}
