b16b5ddfd543ffaf0805e09fd559a1e7d20aec2b
Omkar-kashid-code <omkarkashid.2003@gmail.com>
2024-09-20 22:51:57 +0530

    Update assign2.java

--- Day12/assign2.java
+++ Day12/assign2.java
@@ -4,7 +4,7 @@ class Cemo{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter a first number");
 		int num1 =sc.nextInt();
-		System.out.println("enter a first number");
+		System.out.println("enter a second number");
 		int num2 =sc.nextInt();
 		int multiplication =num1 * num2;
 		System.out.println("Multiplication is ="+ multiplication);
